7d8426424aba4d99d4885ecad2cccf8f84741e4c
Tianhao Chai <cth451@gmail.com>
2022-04-26 23:51:14 -0500

    logging: make critical errors actually critical

--- src/Logging.h
+++ src/Logging.h
@@ -20,16 +20,17 @@ namespace GAPPP {
 		return fmt::format("{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 	};
 
-	inline void whine(Severity sev, const std::string &message) {
+	inline void whine(Severity sev, const std::string &message, const std::string &component = "none") {
 		switch (sev) {
 			case Severity::INFO:
-				std::cout << "INFO " << message << std::endl;
+				std::cout << component << "/INFO " << message << std::endl;
 				return;
 			case Severity::WARN:
-				std::cerr << "INFO " << message << std::endl;
+				std::cout << component << "/WARN " << message << std::endl;
 				return;
 			case Severity::CRIT:
-				std::cerr << "CRIT " << message << std::endl;
+				std::cout << component << "/CRIT " << message << std::endl;
+				throw std::runtime_error(message);
 		}
 	}
 }
