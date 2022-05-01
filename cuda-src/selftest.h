174b4bb18f05ef54e86a14be5c82443592564838
Tianhao Chai <cth451@gmail.com>
2022-05-01 00:41:46 -0500

    gpu: print selftest result

--- cuda-src/selftest.h
+++ cuda-src/selftest.h
@@ -6,9 +6,6 @@
 
 namespace GAPPP::selftest {
 	int invoke(unsigned int nbr_tasks, struct rte_mbuf **packets);
-	inline bool selftest() {
-		return GAPPP::selftest::invoke(0, nullptr) == 255;
-	}
 }
 
 #endif
--- src/Logging.h
+++ src/Logging.h
@@ -20,16 +20,16 @@ namespace GAPPP {
 		return fmt::format("{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 	};
 
-	inline void whine(Severity sev, const std::string &message, const std::string &component = "none") {
+	inline void whine(Severity sev, const std::string &message, const std::string &component = "???") {
 		switch (sev) {
 			case Severity::INFO:
-				std::cout << component << "/INFO " << message << std::endl;
+				std::cout << "[" << component << "/INFO] " << message << std::endl;
 				return;
 			case Severity::WARN:
-				std::cout << component << "/WARN " << message << std::endl;
+				std::cout << "[" << component << "/WARN] " << message << std::endl;
 				return;
 			case Severity::CRIT:
-				std::cout << component << "/CRIT " << message << std::endl;
+				std::cout << "[" << component << "/CRIT] " << message << std::endl;
 				throw std::runtime_error(message);
 		}
 	}
