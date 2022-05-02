cc72b169cf930dcbac95eb285859868b0c716ffa
Samuel Kruse <sdkruse@wisc.edu>
2022-05-02 22:12:45 +0000

    l3fwd

--- cuda-src/l3fwd.cu
+++ cuda-src/l3fwd.cu
@@ -24,17 +24,68 @@
 namespace GAPPP {
 	namespace l3fwd {
 
+
+
 		__managed__ int numroutes = 0;
 		__managed__ struct route routes[4096];
+		__global__ void prefixMatchPacket(struct rte_mbuf *m,  uint32_t **masks){
+			int i = threadIdx.x;
+			
+			route r = routes[i];
+			/* Remove the Ethernet header and trailer from the input packet */
+   			 rte_pktmbuf_adj(m, (uint16_t)sizeof(struct rte_ether_hdr));
+    			
+			/* if this is an IPv4 packet */
+       			struct rte_ipv4_hdr *ip_hdr;
+       			uint32_t ip_dst;
+        		
+			/* Read the lookup key (i.e. ip_dst) from the input packet */
+        		ip_hdr = rte_pktmbuf_mtod(m, struct rte_ipv4_hdr *);
+        		ip_dst = rte_be_to_cpu_32(ip_hdr->dst_addr);
+
+			uint32_t b0,b1,b2,b3;
+			uint32_t res;
+
+			b0 = (ip_dst & 0x000000ff) << 24u;
+			b1 = (ip_dst & 0x0000ff00) << 8u;
+			b2 = (ip_dst & 0x00ff0000) >> 8u;
+			b3 = (ip_dst & 0xff000000) >> 24u;
+
+			res = b0 | b1 | b2 | b3;
+			
+			if(res & r.mask == r.network){
+				masks[i] = r.mask;
+			}
+
+			
+		}
+
 		__global__ void prefixMatch(struct rte_mbuf **packets){
 			int i = threadIdx.x;
-			int out_port = -1;
-			// TODO: processing here
-			// TODO: lookup the table, find longest match
-			packets[i]->port = out_port;
+			rte_mbuf packet = packets[i];
+			uint32_t masks[numroutes];
+			
+			prefixMatchPacket<<<1,numroutes>>>(packet, masks);
+			uint32_t max_mask = 0;
+			int max_ind = -1;
+			for(int j = 0; j<numroutes:j++){
+				
+				uint32_t cur = masks[j];
+
+				if(cur > max_mask){
+					max_mask = cur;
+					max_ind = j;
+
+			}	
+
+			
+			packets[i]->port = routes[max_ind].out_port;
 		}
+
+
+
 		int invoke(unsigned int nbr_tasks, struct rte_mbuf **packets) {
-			prefixMatch<<<1, nbr_tasks>>>(packets);
+			prefixMatch<<<1, nbr_tasks >>>(packets);
 			return 0;
 		}
 
@@ -46,14 +97,12 @@ namespace GAPPP {
 
 		int setTable(GAPPP::routing_table t) {
 
-			//cudaMemcpyToSymbol(routetable, &t, sizeof(t), size_t(0),cudaMemcpyHostToDevice);
 
 			numroutes = t.size();
 			cudaMemcpy(routes, t.data(), sizeof(route) * t.size(), cudaMemcpyHostToDevice);
 
 			readTable<<<1, numroutes>>>();
 
-			//cudaMemcpy(&ret, gpu, sizeof(t), cudaMemcpyDeviceToHost);
 
 			return 0;
 		}
