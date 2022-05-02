80523a94d848336dce6f33201bd0198a9a0c75b4
Tianhao Chai <cth451@gmail.com>
2022-05-02 13:35:54 -0500

    dummy module

---
+++ cuda-src/dummy.cu
@@ -0,0 +1,5 @@
+namespace GAPPP::dummy {
+	int invoke(unsigned int nbr_tasks, struct rte_mbuf **packets) {
+		return 0;
+	}
+}
---
+++ cuda-src/dummy.h
@@ -0,0 +1,18 @@
+//
+// Created by cth451 on 5/2/22.
+//
+
+#ifndef GAPPP_CUDA_DUMMY_H
+#define GAPPP_CUDA_DUMMY_H
+
+namespace GAPPP::dummy {
+	/**
+	 * Dummy processing module - map things into GPU memory and do nothing
+	 * @param nbr_tasks
+	 * @param packets
+	 * @return
+	 */
+	int invoke(unsigned int nbr_tasks, struct rte_mbuf **packets);
+}
+
+#endif //GAPPP_DUMMY_H
